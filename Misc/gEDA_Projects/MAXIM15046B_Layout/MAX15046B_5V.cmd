b67a0ff20dce04813655d919674275abf31c9767
sabinoj <johntsabino@gmail.com>
2015-01-15 19:23:32 -0600

    Complete bill of materials for MAX15046B_5V circuit. As well as updates to the schematic and PCB itself.

---
+++ Misc/gEDA_Projects/MAXIM15046B_Layout/MAX15046B_5V.cmd
@@ -0,0 +1,143 @@
+# Pin name action command file
+
+# Start of element R115
+ChangePinName(R115, 1, 1)
+ChangePinName(R115, 2, 2)
+
+# Start of element CONN101
+ChangePinName(CONN101, 5, 5)
+ChangePinName(CONN101, 3, 3)
+ChangePinName(CONN101, 1, 1)
+ChangePinName(CONN101, 6, 6)
+ChangePinName(CONN101, 4, 4)
+ChangePinName(CONN101, 2, 2)
+
+# Start of element C109
+ChangePinName(C109, 2, 2)
+ChangePinName(C109, 1, 1)
+
+# Start of element D101
+ChangePinName(D101, 2, 2)
+ChangePinName(D101, 1, 1)
+
+# Start of element R103
+ChangePinName(R103, 1, 1)
+ChangePinName(R103, 2, 2)
+
+# Start of element C119
+ChangePinName(C119, 2, 2)
+ChangePinName(C119, 1, 1)
+
+# Start of element C112
+ChangePinName(C112, 2, -)
+ChangePinName(C112, 1, +)
+
+# Start of element L101
+ChangePinName(L101, 1, 1)
+ChangePinName(L101, 2, 2)
+
+# Start of element R107
+ChangePinName(R107, 1, 1)
+ChangePinName(R107, 2, 2)
+
+# Start of element Q101
+
+# Start of element Q102
+
+# Start of element C108
+ChangePinName(C108, 2, 2)
+ChangePinName(C108, 1, 1)
+
+# Start of element C104
+ChangePinName(C104, 2, 2)
+ChangePinName(C104, 1, 1)
+
+# Start of element R105
+ChangePinName(R105, 1, 1)
+ChangePinName(R105, 2, 2)
+
+# Start of element C105
+ChangePinName(C105, 2, 2)
+ChangePinName(C105, 1, 1)
+
+# Start of element C116
+ChangePinName(C116, 2, 2)
+ChangePinName(C116, 1, 1)
+
+# Start of element R111
+ChangePinName(R111, 1, 1)
+ChangePinName(R111, 2, 2)
+
+# Start of element R112
+ChangePinName(R112, 1, 1)
+ChangePinName(R112, 2, 2)
+
+# Start of element R113
+ChangePinName(R113, 1, 1)
+ChangePinName(R113, 2, 2)
+
+# Start of element R114
+ChangePinName(R114, 1, 1)
+ChangePinName(R114, 2, 2)
+
+# Start of element C114
+ChangePinName(C114, 2, 2)
+ChangePinName(C114, 1, 1)
+
+# Start of element C115
+ChangePinName(C115, 2, 2)
+ChangePinName(C115, 1, 1)
+
+# Start of element R110
+ChangePinName(R110, 1, 1)
+ChangePinName(R110, 2, 2)
+
+# Start of element C113
+ChangePinName(C113, 2, 2)
+ChangePinName(C113, 1, 1)
+
+# Start of element R109
+ChangePinName(R109, 1, 1)
+ChangePinName(R109, 2, 2)
+
+# Start of element R104
+ChangePinName(R104, 1, 1)
+ChangePinName(R104, 2, 2)
+
+# Start of element C102
+ChangePinName(C102, 2, 2)
+ChangePinName(C102, 1, 1)
+
+# Start of element C106
+ChangePinName(C106, 2, 2)
+ChangePinName(C106, 1, 1)
+
+# Start of element C107
+ChangePinName(C107, 2, 2)
+ChangePinName(C107, 1, 1)
+
+# Start of element C103
+ChangePinName(C103, 2, 2)
+ChangePinName(C103, 1, 1)
+
+# Start of element C101
+ChangePinName(C101, N, -)
+ChangePinName(C101, P, +)
+
+# Start of element U101
+ChangePinName(U101, 9, GND)
+ChangePinName(U101, 16, CSP)
+ChangePinName(U101, 15, DH)
+ChangePinName(U101, 14, LX)
+ChangePinName(U101, 13, BST)
+ChangePinName(U101, 12, DL)
+ChangePinName(U101, 11, DRV)
+ChangePinName(U101, 10, PGND)
+ChangePinName(U101, 1, IN)
+ChangePinName(U101, 8, RT)
+ChangePinName(U101, 7, FB)
+ChangePinName(U101, 6, COMP)
+ChangePinName(U101, 5, LIM)
+ChangePinName(U101, 4, EN)
+ChangePinName(U101, 3, PGOOD)
+ChangePinName(U101, 2, Vcc)
