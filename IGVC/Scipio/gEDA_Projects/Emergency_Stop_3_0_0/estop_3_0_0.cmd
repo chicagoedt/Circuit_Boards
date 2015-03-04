0d7e2c0cc73eb289b04ae360c5794dda5c2ccded
Andrew Pable <andrewpable@gmail.com>
2015-03-03 19:51:13 -0600

    created and finalized Emergency stop 3.0.0

---
+++ IGVC/Scipio/gEDA_Projects/Emergency_Stop_3_0_0/estop_3_0_0.cmd
@@ -0,0 +1,82 @@
+# Pin name action command file
+
+# Start of element C101
+ChangePinName(C101, 7, +Vout)
+ChangePinName(C101, 6, TRM)
+ChangePinName(C101, 5, -Vout)
+ChangePinName(C101, 4, NC)
+ChangePinName(C101, 1, +Vin)
+ChangePinName(C101, 2, RC)
+ChangePinName(C101, 3, -Vin)
+
+# Start of element W101
+ChangePinName(W101, 13, GND)
+ChangePinName(W101, 14, GND)
+ChangePinName(W101, 16, NC)
+ChangePinName(W101, 15, NC)
+ChangePinName(W101, 17, NC)
+ChangePinName(W101, 18, NC)
+ChangePinName(W101, 19, EX)
+ChangePinName(W101, 20, RSSI)
+ChangePinName(W101, 22, BE)
+ChangePinName(W101, 21, CMD_RSP!)
+ChangePinName(W101, 23, GND)
+ChangePinName(W101, 24, GND)
+ChangePinName(W101, 12, GND)
+ChangePinName(W101, 11, GND)
+ChangePinName(W101, 9, NC)
+ChangePinName(W101, 10, NC)
+ChangePinName(W101, 8, NC)
+ChangePinName(W101, 7, NC)
+ChangePinName(W101, 6, NC)
+ChangePinName(W101, 5, CMD!)
+ChangePinName(W101, 3, TXD)
+ChangePinName(W101, 4, CTS!)
+ChangePinName(W101, 2, RXD)
+ChangePinName(W101, 1, VCC)
+
+# Start of element CONN102
+ChangePinName(CONN102, 1, 1)
+ChangePinName(CONN102, 2, 2)
+
+# Start of element CONN101
+ChangePinName(CONN101, 1, 1)
+ChangePinName(CONN101, 2, 2)
+
+# Start of element D101
+ChangePinName(D101, 2, 2)
+ChangePinName(D101, 1, 1)
+
+# Start of element R101
+ChangePinName(R101, 1, 1)
+ChangePinName(R101, 2, 2)
+
+# Start of element U101
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, 28, "(ADC5/SCL) PC5")
+ChangePinName(U101, 27, "(ADC4/SDA) PC4")
+ChangePinName(U101, 26, "(ADC3) PC3")
+ChangePinName(U101, 25, "(ADC2) PC2")
+ChangePinName(U101, 24, "(ADC1) PC1")
+ChangePinName(U101, 23, "(ADC0) PC0")
+ChangePinName(U101, 21, AREF)
+ChangePinName(U101, 20, AVCC)
+ChangePinName(U101, 19, "PB5 (SCK)")
+ChangePinName(U101, 18, "PB4 (MISO)")
+ChangePinName(U101, 17, "PB3 (MOSI/OC2)")
+ChangePinName(U101, 16, "PB2 (SS/OC1B)")
+ChangePinName(U101, 15, "PB1 (OC1A)")
+ChangePinName(U101, 14, "PB0 (ICP1)")
+ChangePinName(U101, 13, "PD7 (AIN1)")
+ChangePinName(U101, 12, "PD6 (AIN0)")
+ChangePinName(U101, 11, "PD5 (T1)")
+ChangePinName(U101, 10, "PB7 (XTAL2/TOSC2)")
+ChangePinName(U101, 9, "PB6 (XTAL1/OSC1)")
+ChangePinName(U101, 6, "PD4 (XCK/T0)")
+ChangePinName(U101, 5, "PD3 (INT1)")
+ChangePinName(U101, 4, "PD2 (INT0)")
+ChangePinName(U101, 3, "PD1 (TxD)")
+ChangePinName(U101, 2, "PD0 (RxD)")
+ChangePinName(U101, 1, "(RESET) PC6")
