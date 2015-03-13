19c6aea918f891f9aa45dd609ed52f79c3c5bc90
L0g1x-Krystian <krgebis@gmail.com>
2015-03-12 20:51:23 -0500

    Initial 3.1.0 re-design/re-layout that me and ammar have worked on. No longer contains a DIP Atmega32u4 (jumpers, specifically) and instead contains a SoC atmega32u4

---
+++ RMC/Hushpuppy/gEDA_Projects/Signal_Multiplexer_3.1.0/Signal_Multiplexer_3_1_0.cmd
@@ -0,0 +1,300 @@
+# Pin name action command file
+
+# Start of element U109
+ChangePinName(U109, 23, GND)
+ChangePinName(U109, 24, AVcc)
+ChangePinName(U109, 25, "PD4 (ICP1/ADC8)")
+ChangePinName(U109, 26, "PD6 (T1/!OC4D/ADC9)")
+ChangePinName(U109, 27, "PD7 (T0/OC4D/ADC10)")
+ChangePinName(U109, 28, "PB4 (PCINT4/ADC11)")
+ChangePinName(U109, 29, "PB5 (PCINT5/OC1A/!OC4B/ADC12)")
+ChangePinName(U109, 30, "PB6 (PCINT6/OC1B/ADC13)")
+ChangePinName(U109, 31, "PC6 (OC3A/!OC4A)")
+ChangePinName(U109, 32, "PC7 (ICP3/CLK0/OC4A)")
+ChangePinName(U109, 33, "PE2 (!HWB)")
+ChangePinName(U109, 34, Vcc)
+ChangePinName(U109, 35, GND)
+ChangePinName(U109, 36, "PF7 (ADC7/TDI)")
+ChangePinName(U109, 37, "PF6 (ADC6/TDO)")
+ChangePinName(U109, 38, "PF5 (ADC5/TMS)")
+ChangePinName(U109, 39, "PF4 (ADC4/TCK)")
+ChangePinName(U109, 40, "PF1 (ADC1)")
+ChangePinName(U109, 41, "PF0 (ADC0)")
+ChangePinName(U109, 42, AREF)
+ChangePinName(U109, 43, GND)
+ChangePinName(U109, 44, AVcc)
+ChangePinName(U109, 22, "PD5 (XCK1/!CTS)")
+ChangePinName(U109, 21, "PD3 (TXD1/INT3)")
+ChangePinName(U109, 20, "PD2 (RXD1/INT2)")
+ChangePinName(U109, 19, "PD1 (SDA/INT1)")
+ChangePinName(U109, 18, "PD0 (OC0B/SCL/INT0)")
+ChangePinName(U109, 17, XTAL1)
+ChangePinName(U109, 16, XTAL2)
+ChangePinName(U109, 15, GND)
+ChangePinName(U109, 14, Vcc)
+ChangePinName(U109, 13, !RESET)
+ChangePinName(U109, 12, "PB7 (PCINT7/OC0A/OC1C/!RTS)")
+ChangePinName(U109, 11, "PB3 (PDO/PCINT3/MISO)")
+ChangePinName(U109, 10, "PB2 (PDI/PCINT2/MOSI)")
+ChangePinName(U109, 9, "PB1 (PCINT1/SCLK)")
+ChangePinName(U109, 8, "PB0 (SS/PCINT0)")
+ChangePinName(U109, 7, VBus)
+ChangePinName(U109, 6, UCap)
+ChangePinName(U109, 5, UGND)
+ChangePinName(U109, 4, D+)
+ChangePinName(U109, 3, D-)
+ChangePinName(U109, 2, UVcc)
+ChangePinName(U109, 1, "PE6 (INT6/AIN0)")
+
+# Start of element CONN121
+ChangePinName(CONN121, 5, 5)
+ChangePinName(CONN121, 3, 3)
+ChangePinName(CONN121, 1, 1)
+ChangePinName(CONN121, 6, 6)
+ChangePinName(CONN121, 4, 4)
+ChangePinName(CONN121, 2, 2)
+
+# Start of element CONN116
+ChangePinName(CONN116, 5, 5)
+ChangePinName(CONN116, 3, 3)
+ChangePinName(CONN116, 1, 1)
+ChangePinName(CONN116, 6, 6)
+ChangePinName(CONN116, 4, 4)
+ChangePinName(CONN116, 2, 2)
+
+# Start of element CONN120
+ChangePinName(CONN120, 1, 1)
+ChangePinName(CONN120, 4, 4)
+ChangePinName(CONN120, 2, 2)
+ChangePinName(CONN120, 3, 3)
+
+# Start of element CONN119
+ChangePinName(CONN119, 3, 3)
+ChangePinName(CONN119, 1, 1)
+ChangePinName(CONN119, 2, 2)
+
+# Start of element U105
+ChangePinName(U105, 16, Vcc)
+ChangePinName(U105, 15, !E)
+ChangePinName(U105, 14, I0D)
+ChangePinName(U105, 13, I1D)
+ChangePinName(U105, 12, YD)
+ChangePinName(U105, 11, I0C)
+ChangePinName(U105, 10, I1C)
+ChangePinName(U105, 9, YC)
+ChangePinName(U105, 8, GND)
+ChangePinName(U105, 7, YB)
+ChangePinName(U105, 6, I1B)
+ChangePinName(U105, 5, I0B)
+ChangePinName(U105, 4, YA)
+ChangePinName(U105, 3, I1A)
+ChangePinName(U105, 2, I0A)
+ChangePinName(U105, 1, S)
+
+# Start of element C110
+ChangePinName(C110, 2, 2)
+ChangePinName(C110, 1, 1)
+
+# Start of element C109
+ChangePinName(C109, 2, 2)
+ChangePinName(C109, 1, 1)
+
+# Start of element U108
+ChangePinName(U108, 15, USBDP)
+ChangePinName(U108, 16, USBDM)
+ChangePinName(U108, 17, 3V3OUT)
+ChangePinName(U108, 18, GND)
+ChangePinName(U108, 19, RESET#)
+ChangePinName(U108, 20, VCC)
+ChangePinName(U108, 21, GND)
+ChangePinName(U108, 22, CBUS1)
+ChangePinName(U108, 23, CBUS0)
+ChangePinName(U108, 24, NC)
+ChangePinName(U108, 25, AGND)
+ChangePinName(U108, 26, TEST)
+ChangePinName(U108, 27, OSCI)
+ChangePinName(U108, 28, OSCO)
+ChangePinName(U108, 14, CBUS3)
+ChangePinName(U108, 13, CBUS2)
+ChangePinName(U108, 12, CBUS4)
+ChangePinName(U108, 11, CTS#)
+ChangePinName(U108, 10, DCD#)
+ChangePinName(U108, 9, DSR#)
+ChangePinName(U108, 8, NC)
+ChangePinName(U108, 7, GND)
+ChangePinName(U108, 6, RI#)
+ChangePinName(U108, 5, RXD)
+ChangePinName(U108, 4, VCCIO)
+ChangePinName(U108, 3, RTS#)
+ChangePinName(U108, 2, DTR#)
+ChangePinName(U108, 1, TXD)
+
+# Start of element R115
+ChangePinName(R115, 1, 1)
+ChangePinName(R115, 2, 2)
+
+# Start of element R114
+ChangePinName(R114, 1, 1)
+ChangePinName(R114, 2, 2)
+
+# Start of element LED112
+ChangePinName(LED112, 2, K)
+ChangePinName(LED112, 1, A)
+
+# Start of element LED111
+ChangePinName(LED111, 2, K)
+ChangePinName(LED111, 1, A)
+
+# Start of element C114
+ChangePinName(C114, 2, 2)
+ChangePinName(C114, 1, 1)
+
+# Start of element C113
+ChangePinName(C113, 2, 2)
+ChangePinName(C113, 1, 1)
+
+# Start of element CONN115
+ChangePinName(CONN115, 5, 5)
+ChangePinName(CONN115, 3, 3)
+ChangePinName(CONN115, 1, 1)
+ChangePinName(CONN115, 4, 4)
+ChangePinName(CONN115, 2, 2)
+
+# Start of element R116
+ChangePinName(R116, 1, 1)
+ChangePinName(R116, 2, 2)
+
+# Start of element D101
+ChangePinName(D101, 2, 2)
+ChangePinName(D101, 1, 1)
+
+# Start of element CONN118
+ChangePinName(CONN118, 1, 1)
+ChangePinName(CONN118, 2, 2)
+
+# Start of element CONN117
+ChangePinName(CONN117, 5, 5)
+ChangePinName(CONN117, 3, 3)
+ChangePinName(CONN117, 1, 1)
+ChangePinName(CONN117, 6, 6)
+ChangePinName(CONN117, 4, 4)
+ChangePinName(CONN117, 2, 2)
+
+# Start of element CONN112
+ChangePinName(CONN112, 1, 1)
+ChangePinName(CONN112, 2, 2)
+
+# Start of element CONN111
+ChangePinName(CONN111, 1, 1)
+ChangePinName(CONN111, 2, 2)
+
+# Start of element R109
+ChangePinName(R109, 1, 1)
+ChangePinName(R109, 2, 2)
+
+# Start of element R108
+ChangePinName(R108, 1, 1)
+ChangePinName(R108, 2, 2)
+
+# Start of element LED106
+ChangePinName(LED106, 2, K)
+ChangePinName(LED106, 1, A)
+
+# Start of element LED105
+ChangePinName(LED105, 2, K)
+ChangePinName(LED105, 1, A)
+
+# Start of element CONN110
+ChangePinName(CONN110, 1, 1)
+ChangePinName(CONN110, 2, 2)
+
+# Start of element CONN109
+ChangePinName(CONN109, 1, 1)
+ChangePinName(CONN109, 2, 2)
+
+# Start of element CONN108
+ChangePinName(CONN108, 1, 1)
+ChangePinName(CONN108, 2, 2)
+
+# Start of element CONN107
+ChangePinName(CONN107, 1, 1)
+ChangePinName(CONN107, 2, 2)
+
+# Start of element CONN106
+ChangePinName(CONN106, 1, 1)
+ChangePinName(CONN106, 2, 2)
+
+# Start of element CONN105
+ChangePinName(CONN105, 1, 1)
+ChangePinName(CONN105, 2, 2)
+
+# Start of element J103
+ChangePinName(J103, 6, 6)
+ChangePinName(J103, 7, 7)
+ChangePinName(J103, 8, 8)
+ChangePinName(J103, 9, 9)
+ChangePinName(J103, 10, 10)
+ChangePinName(J103, 5, 5)
+ChangePinName(J103, 1, 1)
+ChangePinName(J103, 4, 4)
+ChangePinName(J103, 3, 3)
+ChangePinName(J103, 2, 2)
+
+# Start of element C102
+ChangePinName(C102, 2, 2)
+ChangePinName(C102, 1, 1)
+
+# Start of element C101
+ChangePinName(C101, 2, 2)
+ChangePinName(C101, 1, 1)
+
+# Start of element U101
+ChangePinName(U101, 2, 2)
+ChangePinName(U101, 1, 1)
+
+# Start of element R105
+ChangePinName(R105, 1, 1)
+ChangePinName(R105, 2, 2)
+
+# Start of element R104
+ChangePinName(R104, 1, 1)
+ChangePinName(R104, 2, 2)
+
+# Start of element R103
+ChangePinName(R103, 1, 1)
+ChangePinName(R103, 2, 2)
+
+# Start of element R102
+ChangePinName(R102, 1, 1)
+ChangePinName(R102, 2, 2)
+
+# Start of element LED104
+ChangePinName(LED104, 2, K)
+ChangePinName(LED104, 1, A)
+
+# Start of element LED103
+ChangePinName(LED103, 2, K)
+ChangePinName(LED103, 1, A)
+
+# Start of element LED102
+ChangePinName(LED102, 2, K)
+ChangePinName(LED102, 1, A)
+
+# Start of element LED101
+ChangePinName(LED101, 2, K)
+ChangePinName(LED101, 1, A)
+
+# Start of element R101
+ChangePinName(R101, 1, 1)
+ChangePinName(R101, 2, 2)
+
+# Start of element J101
+ChangePinName(J101, 9, 9)
+ChangePinName(J101, 7, 7)
+ChangePinName(J101, 5, 5)
+ChangePinName(J101, 3, 3)
+ChangePinName(J101, 1, 1)
+ChangePinName(J101, 8, 8)
+ChangePinName(J101, 6, 6)
+ChangePinName(J101, 4, 4)
+ChangePinName(J101, 2, 2)
