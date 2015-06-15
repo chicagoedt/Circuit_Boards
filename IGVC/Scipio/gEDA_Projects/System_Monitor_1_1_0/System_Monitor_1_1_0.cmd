6dcc726cca11c08ac94d6b8104496462c0c1459d
sabinoj <johntsabino@gmail.com>
2015-06-15 13:10:32 -0500

    Created System Monitor Card 1.1.0 with corrected ADC pins, and a decoupling capacitor.

---
+++ IGVC/Scipio/gEDA_Projects/System_Monitor_1_1_0/System_Monitor_1_1_0.cmd
@@ -0,0 +1,197 @@
+# Pin name action command file
+
+# Start of element C104
+ChangePinName(C104, 2, 2)
+ChangePinName(C104, 1, 1)
+
+# Start of element D105
+ChangePinName(D105, 2, 2)
+ChangePinName(D105, 1, 1)
+
+# Start of element R109
+ChangePinName(R109, 1, 1)
+ChangePinName(R109, 2, 2)
+
+# Start of element R108
+ChangePinName(R108, 1, 1)
+ChangePinName(R108, 2, 2)
+
+# Start of element D104
+ChangePinName(D104, 2, 2)
+ChangePinName(D104, 1, 1)
+
+# Start of element D103
+ChangePinName(D103, 2, 2)
+ChangePinName(D103, 1, 1)
+
+# Start of element R107
+ChangePinName(R107, 1, 1)
+ChangePinName(R107, 2, 2)
+
+# Start of element D102
+ChangePinName(D102, 2, 2)
+ChangePinName(D102, 1, 1)
+
+# Start of element R106
+ChangePinName(R106, 1, 1)
+ChangePinName(R106, 2, 2)
+
+# Start of element C103
+ChangePinName(C103, 2, 2)
+ChangePinName(C103, 1, 1)
+
+# Start of element D101
+ChangePinName(D101, 2, 2)
+ChangePinName(D101, 1, 1)
+
+# Start of element R105
+ChangePinName(R105, 1, 1)
+ChangePinName(R105, 2, 2)
+
+# Start of element F101
+ChangePinName(F101, 2, 2)
+ChangePinName(F101, 1, 1)
+
+# Start of element J101
+ChangePinName(J101, 6, 6)
+ChangePinName(J101, 7, 7)
+ChangePinName(J101, 8, 8)
+ChangePinName(J101, 9, 9)
+ChangePinName(J101, 10, 10)
+ChangePinName(J101, 5, 5)
+ChangePinName(J101, 1, 1)
+ChangePinName(J101, 4, 4)
+ChangePinName(J101, 3, 3)
+ChangePinName(J101, 2, 2)
+
+# Start of element R104
+ChangePinName(R104, 1, 1)
+ChangePinName(R104, 2, 2)
+
+# Start of element C102
+ChangePinName(C102, 2, 2)
+ChangePinName(C102, 1, 1)
+
+# Start of element C101
+ChangePinName(C101, 2, 2)
+ChangePinName(C101, 1, 1)
+
+# Start of element U102
+ChangePinName(U102, 2, 2)
+ChangePinName(U102, 1, 1)
+
+# Start of element U101
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, unknown, unknown)
+ChangePinName(U101, 40, "(ADC0) PA0")
+ChangePinName(U101, 39, "(ADC1) PA1")
+ChangePinName(U101, 38, "(ADC2) PA2")
+ChangePinName(U101, 37, "(ADC3) PA3")
+ChangePinName(U101, 36, "(ADC4) PA4")
+ChangePinName(U101, 35, "(ADC5) PA5")
+ChangePinName(U101, 34, "(ADC6) PA6")
+ChangePinName(U101, 33, "(ADC7) PA7")
+ChangePinName(U101, 22, "(SCL) PC0")
+ChangePinName(U101, 23, "(SDA) PC1")
+ChangePinName(U101, 24, "PC2 (TCK)")
+ChangePinName(U101, 25, "PC3 (TMS)")
+ChangePinName(U101, 26, "PC4 (TDO)")
+ChangePinName(U101, 27, "PC5 (TDI)")
+ChangePinName(U101, 28, "(TOSC1) PC6")
+ChangePinName(U101, 29, "(TOSC2) PC7")
+ChangePinName(U101, 1, "PB0 (XCK/T0)")
+ChangePinName(U101, 2, "PB1 (T1)")
+ChangePinName(U101, 3, "PB2 (INT2/AIN0)")
+ChangePinName(U101, 4, "PB3 (OC0/AIN1)")
+ChangePinName(U101, 5, "PB4 (\_SS\_)")
+ChangePinName(U101, 6, "PB5 (MOSI)")
+ChangePinName(U101, 7, "PB6 (MISO)")
+ChangePinName(U101, 8, "PB7 (SCK)")
+ChangePinName(U101, 14, "PD0 (RXD)")
+ChangePinName(U101, 15, "PD1 (TXD)")
+ChangePinName(U101, 16, "PD2 (INT0)")
+ChangePinName(U101, 17, "PD3 (INT1)")
+ChangePinName(U101, 18, "PD4 (OC1B)")
+ChangePinName(U101, 19, "PD5 (OC1A)")
+ChangePinName(U101, 20, "PD6 (ICP1)")
+ChangePinName(U101, 21, "PD7 (OC2)")
+ChangePinName(U101, 32, AREF)
+ChangePinName(U101, 13, XTAL1)
+ChangePinName(U101, 12, XTAL2)
+ChangePinName(U101, 9, \_Reset\_)
+
+# Start of element CONN101
+ChangePinName(CONN101, 26, 26)
+ChangePinName(CONN101, 30, 30)
+ChangePinName(CONN101, 29, 29)
+ChangePinName(CONN101, 28, 28)
+ChangePinName(CONN101, 27, 27)
+ChangePinName(CONN101, 21, 21)
+ChangePinName(CONN101, 25, 25)
+ChangePinName(CONN101, 24, 24)
+ChangePinName(CONN101, 23, 23)
+ChangePinName(CONN101, 22, 22)
+ChangePinName(CONN101, 16, 16)
+ChangePinName(CONN101, 20, 20)
+ChangePinName(CONN101, 19, 19)
+ChangePinName(CONN101, 18, 18)
+ChangePinName(CONN101, 17, 17)
+ChangePinName(CONN101, 11, 11)
+ChangePinName(CONN101, 15, 15)
+ChangePinName(CONN101, 14, 14)
+ChangePinName(CONN101, 13, 13)
+ChangePinName(CONN101, 12, 12)
+ChangePinName(CONN101, 6, 6)
+ChangePinName(CONN101, 10, 10)
+ChangePinName(CONN101, 9, 9)
+ChangePinName(CONN101, 8, 8)
+ChangePinName(CONN101, 7, 7)
+ChangePinName(CONN101, 1, 1)
+ChangePinName(CONN101, 5, 5)
+ChangePinName(CONN101, 4, 4)
+ChangePinName(CONN101, 3, 3)
+ChangePinName(CONN101, 2, 2)
+ChangePinName(CONN101, 56, 56)
+ChangePinName(CONN101, 60, 60)
+ChangePinName(CONN101, 59, 59)
+ChangePinName(CONN101, 58, 58)
+ChangePinName(CONN101, 57, 57)
+ChangePinName(CONN101, 51, 51)
+ChangePinName(CONN101, 55, 55)
+ChangePinName(CONN101, 54, 54)
+ChangePinName(CONN101, 53, 53)
+ChangePinName(CONN101, 52, 52)
+ChangePinName(CONN101, 46, 46)
+ChangePinName(CONN101, 50, 50)
+ChangePinName(CONN101, 49, 49)
+ChangePinName(CONN101, 48, 48)
+ChangePinName(CONN101, 47, 47)
+ChangePinName(CONN101, 41, 41)
+ChangePinName(CONN101, 45, 45)
+ChangePinName(CONN101, 44, 44)
+ChangePinName(CONN101, 43, 43)
+ChangePinName(CONN101, 42, 42)
+ChangePinName(CONN101, 36, 36)
+ChangePinName(CONN101, 40, 40)
+ChangePinName(CONN101, 39, 39)
+ChangePinName(CONN101, 38, 38)
+ChangePinName(CONN101, 37, 37)
+ChangePinName(CONN101, 31, 31)
+ChangePinName(CONN101, 35, 35)
+ChangePinName(CONN101, 34, 34)
+ChangePinName(CONN101, 33, 33)
+ChangePinName(CONN101, 32, 32)
+
+# Start of element R103
+ChangePinName(R103, 1, 1)
+ChangePinName(R103, 2, 2)
+
+# Start of element R102
+ChangePinName(R102, 1, 1)
+ChangePinName(R102, 2, 2)
+
+# Start of element R101
+ChangePinName(R101, 1, 1)
+ChangePinName(R101, 2, 2)
